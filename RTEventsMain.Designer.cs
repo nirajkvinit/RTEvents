45a6611804f2b7f00230e8ee0b6fc250aeb380e1
nirajkvinit <nirajkvinit@yahoo.co.in>
2017-04-23 22:45:52 +0530

    Initial Commit

---
+++ RTEventsMain.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace RTEvents
+{
+    partial class RTEventsMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RTEventsMain));
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lbRTShow = new System.Windows.Forms.ListBox();
+            this.rtTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtWebUrl = new System.Windows.Forms.TextBox();
+            this.cmdSaveWebsite = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(174, 6);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(159, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tabControl1);
+            this.groupBox2.Controls.Add(this.lblState);
+            this.groupBox2.Location = new System.Drawing.Point(8, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(367, 110);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fingerprint Device Connection ";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(6, 20);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(355, 78);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Controls.Add(this.txtIP);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.btnConnect);
+            this.tabPage1.Controls.Add(this.txtPort);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tabPage1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(347, 52);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "TCP/IP";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(73, 8);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(95, 20);
+            this.txtIP.TabIndex = 6;
+            this.txtIP.Text = "192.168.1.201";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port";
+            this.label2.Visible = false;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(268, 35);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(53, 20);
+            this.txtPort.TabIndex = 7;
+            this.txtPort.Text = "4370";
+            this.txtPort.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IP Address:";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.ForeColor = System.Drawing.Color.Crimson;
+            this.lblState.Location = new System.Drawing.Point(7, 85);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(138, 13);
+            this.lblState.TabIndex = 2;
+            this.lblState.Text = "Current State:Disconnected";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lbRTShow);
+            this.groupBox3.Location = new System.Drawing.Point(8, 210);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(367, 223);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Fingerprint Device Logger (Shows Activity)";
+            // 
+            // lbRTShow
+            // 
+            this.lbRTShow.FormattingEnabled = true;
+            this.lbRTShow.HorizontalScrollbar = true;
+            this.lbRTShow.Location = new System.Drawing.Point(6, 17);
+            this.lbRTShow.Name = "lbRTShow";
+            this.lbRTShow.ScrollAlwaysVisible = true;
+            this.lbRTShow.Size = new System.Drawing.Size(351, 199);
+            this.lbRTShow.TabIndex = 4;
+            // 
+            // rtTimer
+            // 
+            this.rtTimer.Tick += new System.EventHandler(this.rtTimer_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmdSaveWebsite);
+            this.groupBox1.Controls.Add(this.txtWebUrl);
+            this.groupBox1.Location = new System.Drawing.Point(8, 124);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 80);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Website URL";
+            // 
+            // txtWebUrl
+            // 
+            this.txtWebUrl.Location = new System.Drawing.Point(6, 23);
+            this.txtWebUrl.Name = "txtWebUrl";
+            this.txtWebUrl.Size = new System.Drawing.Size(351, 20);
+            this.txtWebUrl.TabIndex = 0;
+            // 
+            // cmdSaveWebsite
+            // 
+            this.cmdSaveWebsite.Location = new System.Drawing.Point(278, 49);
+            this.cmdSaveWebsite.Name = "cmdSaveWebsite";
+            this.cmdSaveWebsite.Size = new System.Drawing.Size(75, 23);
+            this.cmdSaveWebsite.TabIndex = 1;
+            this.cmdSaveWebsite.Text = "Save Website";
+            this.cmdSaveWebsite.UseVisualStyleBackColor = true;
+            this.cmdSaveWebsite.Click += new System.EventHandler(this.cmdSaveWebsite_Click);
+            // 
+            // RTEventsMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(387, 448);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "RTEventsMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Demo Application for Fingerprint Verification...";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.RTEventsMain_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Timer rtTimer;
+        private System.Windows.Forms.ListBox lbRTShow;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtWebUrl;
+        private System.Windows.Forms.Button cmdSaveWebsite;
+    }
+}
+
